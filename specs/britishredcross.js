45cb933d3ec0941b5688ae9d235e1d152a4686c1
ExuberantGunveer <96468528+ExuberantGunveer@users.noreply.github.com>
2022-04-13 18:27:29 +0530

    Add files via upload

--- specs/britishredcross.js
+++ specs/britishredcross.js
@@ -33,44 +33,114 @@ describe('login test',()=>{
         console.log('Doesnt Match');
     }
 
-    // let itemCount = 0;
-    // const nextBtn = await $("//a[contains(text(),'Next')]");
-    // while(nextBtn.isDisplayed){
-    //     const iteamDiv = await $("//div[class='product-item product-item--vertical  1/3--tablet-and-up 1/3--desk']");
-    //     itemCount = itemCount + iteamDiv.getSize() ;
-    //     nextBtn.click();
-      
-    //     if(!nextBtn.isDisplayed){
-    //         break;
-    //     }
-    // }
 
+    const productOne = await $("//a[contains(text(),'Thomas Street Lime and Basil Leaf Soy Wax Scented Candle')]");
+    await productOne.scrollIntoView();
+    const productOneName = await productOne.getText;
+    console.log("My Product Value is : "+productOneName);
+    await productOne.waitForClickable();
+    await productOne.click();
 
-    // if(nextBtn.isDisplayed){
-    //     const iteamDiv = await $("//div[class='product-item product-item--vertical  1/3--tablet-and-up 1/3--desk']");
-    //     itemCount = itemCount + iteamDiv.getSize() ;
-    //     nextBtn.click();
-    // }else{
-    //     browser.closeWindow();
-    // }
-
-    // })
-
-    // const productOne = await $("//div[class='product-item__info-inner']//a[contains(text(),'Thomas Street Lime and Basil Leaf Soy Wax Scented Candle')]//following-sibling::div//span[class='price']");
-    // await productOne.scrollIntoView();
-    // const productOneName = await productOne.getText;
-    // console.log("My Product Value is : "+productOneName);
-
-
-    // const productOneClick = $("//div[class='product-item__info-inner']//a[contains(text(),'Thomas Street Lime and Basil Leaf Soy Wax Scented Candle')]");
-    // await (await productOneClick).click();
-     
-    // await  browser.pause(3000);
+    const addtocartbtn = await $("(//button[contains(text(),'Add to cart')])[1]");
+    await addtocartbtn.waitForClickable();
+    await addtocartbtn.click();
     
-    // const productOne1 = await $("//div[class='product-form__info-content']//div[class='price-list']//span[class='price']");
-    // const productOneName1 = await productOne1.getText;
-    // console.log("My Product Value is : "+productOneName1);
+    const newinLink = $("(//a[contains(text(),'New In')])[2]");
+    await newinLink.waitForClickable();
+    await newinLink.click();
 
+    await  browser.pause(2000);
+
+
+    const productTwo = await $("//a[contains(text(),'Thomas Street Pomegranate Soy Wax Scented Candle')]");
+    await productTwo.scrollIntoView();
+    await productTwo.waitForClickable();
+    await productTwo.click();
+
+    await addtocartbtn.waitForClickable();
+    await addtocartbtn.click();
+
+
+    const cart = await $("//span[contains(text(),'Cart')]");
+    await cart.waitForClickable();
+    await cart.click();
+    await browser.pause(3000);
+
+    // async finalvalue => {
+
+    const finalAddToCartPrice = await $("//span[@class='cart-recap__price-line-price']");
+    const kk = await finalAddToCartPrice.getText();
+    console.log("String Total Product Price is : - "+ kk);
+   const productFinalIntPrice =  kk.replace('£', '');
+  
+     console.log("The Total Product Price is :- " + productFinalIntPrice);
+     let one =  await Math.floor(productFinalIntPrice);
+    console.log("Int Value : "+ one);
+        // return productFinalIntPrice ;
+    // }
+
+// async addToNumber =>{
+    const productOnePrice = await $("(//span[@class='line-item__price'])[1]");
+    const productTwoPrice = await $("(//span[@class='line-item__price'])[2]");
+
+    // Get text of 1st Product Value & Converting String to Integer :-
+    const po = await productOnePrice.getText();
+    console.log("String Product Price is : - "+ po);
+    const productOneIntPrice =  po.replace('£', '');
+    let two = await Math.floor(productOneIntPrice);
+    console.log("First Product Int Value : " + two)
+
+    // Get text of 2nd Product Value & Converting String to Integer :-
+    const pt = await productTwoPrice.getText();
+    console.log("String Product Price is : - "+ pt);
+    const productTwoIntPrice =  pt.replace('£', '');
+    let three = await Math.floor(productTwoIntPrice);
+    console.log("Second Product Int Value : " +three);
+
+    // Verifying Total Value of Product with the Value of Products Added to Cart
+    let sum = two + three ;
+    console.log("Total Sum of Product is : " + sum);
+
+    if(sum=== one){
+        console.log("HURREEEYYYYYY ! The Product Value Match with Total Cart Value");
+    }else {
+        console.log("Sorry : The Product Value Doesn't Match with Cart Value");
+    }
+
+
+// }
+
+// Verifying First Product Quantity Value & Converting String to Integer : 
+  const quantityOne = await $("(//input[@class='quantity-selector__value'])[2]");
+  const qo = await quantityOne.getAttribute('value');
+  console.log("Quantity of First Product is : "+ qo);
+  let qone = await Math.floor(qo);
+  console.log("Integer Value of Product One : " + qone);
+
+
+// Verifying Second  Product Quantity Value & Converting String to Integer :
+  const quantityTwo = await $("(//input[@class='quantity-selector__value'])[4]");
+  const qt = await quantityTwo.getAttribute('value');
+  console.log("Quantity of Second Product is : "+ qt);
+  let qtwo = await Math.floor(qt);
+  console.log("Integer Value of Product Two : " + qtwo);
+
+// Verifying Product Quantity Value shown on Cart & Converting String to Integer :  
+  const cartValue = await $("//span[@class='header__cart-count']");
+  const cv = await cartValue.getText();
+  console.log("Products added to cart : " + cv);
+  let qcv = await Math.floor(cv);
+  console.log("Integer Value of Added Product to Cart : " + qcv);
+
+  // Addition of First & Second Product Quantity Value : 
+  let sumCart = qone + qtwo ;
+
+  // Verifying ! The Individual Product Value Match with Total Cart Value : 
+  if(sumCart === qcv){
+      console.log(" YIPPEEEEEE !  The Individual Product Value Match with Total Cart Value ");
+  } else {
+      console.log("Sorry : The Individual Product Value Doesn't Match with Cart Value")
+  }
 
 
 })
